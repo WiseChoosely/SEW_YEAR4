49d53ff941ab7c5b11f63694b8837e7ff6133b55
holzer <dominik.holzer@htl.rennweg.at>
2021-11-22 00:05:02 +0100

    Erste Komponente ertsellen

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <md-card>
+            <md-toolbar class="md-primary">
+                <h3 class="md-title">{{title}}</h3>
+            </md-toolbar>
+        <md-card-content>
+        <md-field>
+            <label>{{placeholderEmail}}</label>
+            <md-input></md-input>
+        </md-field>
+
+         <md-field>
+            <label>{{placeholderPassword}}</label>
+            <md-input></md-input>
+        </md-field>
+        </md-card-content>
+        <md-card-actions>
+            <div class= "md-layout md-alignment-left">
+                <md-button class="md-raised md-primary">{{buttonText}}</md-button>
+            </div>
+        </md-card-actions>
+        </md-card>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'Login',
+    props: {
+        title: String,
+        placeholderEmail: String,
+        placeholderPassword: String, 
+        buttonText: String,
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ReihungRadio.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <md-radio v-model="radio" value="value1" disabled="false">1</md-radio>
+    <md-radio v-model="radio" value="value2">2</md-radio>
+    <md-radio v-model="radio" value="value3">3</md-radio>
+    <md-radio v-model="radio" value="value4">4</md-radio>
+    <md-radio v-model="radio" value="vlaue5">5</md-radio>
+  </div>
+</template>
+
+<script>
+
+export default{
+    name: 'ReihungRadio',
+    props: {
+        name: String,
+       
+    }
+}
+
+</script>
+
+<style scoped>
+
+</style>
